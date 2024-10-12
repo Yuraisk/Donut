eaabae6651977c6be7b952d71acb64cf047b7edd
Yra <iskorostenskiura@gmail.com>
2024-10-12 17:45:13 +0200

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	int width = 120;
+	int height = 30;
+	float aspect = (float)width / height;
+	float pixelAspect = 11.0f / 24.0f;
+
+	float outerRadius = 0.7f;
+	float innerRadius = 0.3f;
+
+	char* screen = new char[width * height + 1];
+	screen[width * height] = '\0';
+
+	for (int i = 0; i < width; i++)
+	{
+		for (int j = 0; j < height; j++)
+		{
+			float x = (float)i / width * 2.0f - 1.0f;
+			float y = (float)j / height * 2.0f - 1.0f;
+			x *= aspect * pixelAspect;
+			float distance = x * x + y * y;
+			char pixel = ' ';
+			if (distance < outerRadius * outerRadius && distance > innerRadius * innerRadius)
+			{
+				pixel = '@';
+			}
+			screen[i + j * width] = pixel;
+		}
+	}
+
+	cout << screen;
+
+	delete[] screen;
+
+	return 0;
+}
